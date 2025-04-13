1968647b6e165d437dfceb1135c61073e71a91b2
giocampos2024 <gio-campos@outlook.com.br>
2025-04-13 15:49:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(
     session({
-        secret: "3d8c4b1f89a74e9c52f58f69e90a6b3d23f64b32c7a889e4cb9f2d86c7e4a2b3",
+        secret: "5cd5981e0b8d1cae27f5f8432b4dbfd28bb16638c998203c824f9b1948b8f54d",
         resave: false,
         saveUninitialized: true,
         cookie: { secure: false }, // Secure should be true in production with HTTPS
